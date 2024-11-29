c1e8276ec77590d97661fcaba72eebcb29413afe
Brandon Assing <assing.brandon@gmail.com>
2024-11-29 10:36:36 -0500

    Initial Commit

---
+++ force-gauge/force_gaugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  force_gaugeApp.swift
+//  force-gauge
+//
+//  Created by Brandon Assing on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct force_gaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ force-gaugeTests/force_gaugeTests.swift
@@ -0,0 +1,17 @@
+//
+//  force_gaugeTests.swift
+//  force-gaugeTests
+//
+//  Created by Brandon Assing on 2024-11-29.
+//
+
+import Testing
+@testable import force_gauge
+
+struct force_gaugeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
